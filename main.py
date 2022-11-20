73eab1e40570ffe8dcefc1b32179c678d06557e4
sibapochio <max.pl.pochio@gmail.com>
2022-11-20 15:37:14 +0900

    Ver1.0

--- main.py
+++ main.py
@@ -14,8 +14,6 @@ xbee.atcmd('AV', 0x00)  # ADC_reference_1.25V
 temp = machine.ADC('D1')
 sl = str(binascii.hexlify(xbee.atcmd('SL')))[6:-1]
 print(sl)
-time.sleep(1)
-
 drv = ["""&{'temp':'25','o_time':'07:00','c_time':'16:00','select':'21','wall':'21','everyday':'10',\
 'remote':'10','button':'01'}""", """<body><h1><strong>くるファミ　AceDX</strong></h1><form name="Form" method="POST">\
 <input name="cont_dev" value='""" + sl + """' hidden>""", """<p><input type="radio" id="select1" name="select" \
@@ -43,22 +41,11 @@ exe_dict['wall'] = '{:0>2}'.format(form.getvalue('wall')) if form.getvalue('wall
        """>if 'button' in form:exe_dict['button'] = '{:0>2}'.format(form.getvalue('button'))@@""", '@']
 
 
-# drv.append("""<script>function sel1(ischecked){if(ischecked == true){document.getElementById("se1").disabled = false;\
-# document.getElementById("se2").disabled = true;} else {document.getElementById("se1").disabled = \
-# true;document.getElementById("se2").disabled = false; }}""")
-# drv.append("""<script>function sel2(ischecked){if(ischecked == true){\
-#            document.getElementById("se2").disabled = false;\
-#            document.getElementById("se1").disabled = true;} else {""")
-# drv.append("""<script>document.getElementById("se2").disabled = true;\
-#            document.getElementById("se1").disabled = false;}}</script></body></html>""")
-
-
 def xb_join():
     while True:
         status = xbee.atcmd('AI')  # network_join
         print('.', end='')
         if status == 0x00:
-            # status = 0x01
             print('\nJoin!')
             break
         xbee.atcmd('CB', 0x01)  # commissioning_1
@@ -71,12 +58,11 @@ def pin_ini():
     xbee.atcmd('d6', OFF)
 
 
-def packet_receive(com):
+def packet_receive():
+    payload = ''
     packet = xbee.receive()
     if packet:
         payload = str(packet['payload'].decode('utf-8'))
-    else:
-        payload = com
     return payload
 
 
@@ -105,19 +91,24 @@ def main():
     d = True
     p = True
     t0 = 0
-    s_w = 0
     mes_c = 'C0100001リモート　OFF'
     conf = ''
-    c1 = True
+    c = True
+    t_w = False
     w_s = True
+    o = True
     time_w = 0
     k_time = 0
+    off_time = 0
+    so_time = ''
+    sc_time = ''
     try:
         f = uio.open('conf.txt', mode='r')
         conf = f.read()
         f.close()
-    except OSError as e2:
-        print('初期化ファイル読み込み失敗', e2)
+    except:
+        print('設定ファイルがありません')
+    xbee.transmit(addr_coordinator, 'S')
     while True:
         t = time.ticks_ms()
         if t0 <= t or t0 - t >= 3000:  #
@@ -126,19 +117,18 @@ def main():
                 now_time = 0
                 p_time = 0
             now_time = now_time + 1
-            s_w = 0
             s_time = int(now_time / 60)
-        temp_a = temp.read() * 0.030525 - 48.3
-        command = packet_receive(conf)
-        conf = ''
+        command = packet_receive()
+        if c:
+            command = conf
+            c = False
         if command:
             print(command)
             if command == 'sibainu':
                 send_driver()
-            elif command[0:2] == '99':
+            if command[0:2] == '99':
                 now_time = int(command[-6:])
                 s_time = int(now_time / 60)
-                c1 = False
             else:
                 try:
                     now_time = int(command[-6:])
@@ -151,126 +141,127 @@ def main():
                     everyday = command[16:18]
                     remote = command[18:20]
                     button = command[20:22]
+                    so_time = command[2:7]
+                    sc_time = command[7:12]
                     d = True
                     try:
                         os.remove('conf.txt')
-                    except OSError as e4:
-                        print('初期化ファイル消去失敗', e4)
+                    except:
+                        pass
                     f = uio.open('conf.txt', mode='w')
                     f.write(command)
                     f.close()
-                    if c1:
-                        try:
-                            time.sleep(5)
-                            xbee.transmit(addr_coordinator, 'S')
-                            print('time calibration')
-                        except OSError as e2:
-                            print('時刻取得失敗', e2)
-                except Exception as e3:
-                    print('コマンド代入エラー', e3)
+                except:
+                    temp_c = int(conf[0:2])
+                    o_time = int(conf[2:4]) * 60 + int(conf[5:7])
+                    c_time = int(conf[7:9]) * 60 + int(conf[10:12])
+                    select = conf[12:14]
+                    wall = conf[14:16]
+                    everyday = conf[16:18]
+                    remote = conf[18:20]
+                    button = conf[20:22]
+                    so_time = command[2:7]
+                    sc_time = command[7:12]
             if remote == '11':
                 xbee.atcmd('d6', ON)
-                mes_c = 'リモート　ON'
+                mes_c = 'C0100001リモート　ON'
                 if select == '21':
                     if button == '02':
                         xbee.atcmd('d3', OFF)
                         xbee.atcmd('d2', ON)
                         print('OPEN')
-                    elif button == '03':
+                    if button == '03':
                         xbee.atcmd('d2', OFF)
                         xbee.atcmd('d3', ON)
                         print('CLOSE')
-                    elif button == '04':
+                    if button == '04':
                         xbee.atcmd('d2', OFF)
                         xbee.atcmd('d3', OFF)
                         print('OFF')
-                elif select == '22':
+                if select == '22':
                     if wall == '21':
                         mes_c = "C0100001巻上温度:" + str(temp_c) + '℃'
-
                     if wall == '22' and (d or everyday == '11'):
-                        mes_c = "C0100001巻上時間:" + '(' + command[2:7] + '-' + command[7:12] + ')'
-
-            else:
+                        mes_c = "C0100001巻上時間:" + '(' + so_time + '-' + sc_time + ')'
+            if select == '10':
                 xbee.atcmd('d6', OFF)
                 mes_c = 'C0100001リモート　OFF'
+        if time_w <= now_time and t_w:
+            xbee.atcmd('d2', OFF)
+            xbee.atcmd('d3', OFF)
+            t_w = False
+            w_s = True
+            k_time = s_time + 5
         if p_time <= now_time:
             p_time = now_time + 30
+            temp_a = temp.read() * 0.030525 - 48.3
             if remote == '11' and select == '22':  # 自動
                 if wall == '21':  # 温度
                     if temp_c <= temp_a and k_time <= s_time:
                         xbee.atcmd('d3', OFF)
                         xbee.atcmd('d2', ON)
-                        print('o_open')
                         if w_s:
-                            time_w = now_time + 5
+                            print('temp_open')
+                            time_w = now_time + 3
                             w_s = False
-                        if time_w <= now_time:
-                            xbee.atcmd('d2', OFF)
-                            w_s = True
-                            k_time = s_time + 5
-                    elif temp_c >= temp_a + 5 and k_time <= s_time:  # ヒステリシス　5
+                            t_w = True
+                    if temp_c >= temp_a - 5 and k_time <= s_time:  # ヒステリシス　5
                         xbee.atcmd('d2', OFF)
                         xbee.atcmd('d3', ON)
-                        print('o_close')
                         if w_s:
-                            time_w = now_time + 5
+                            print('temp_close')
+                            time_w = now_time + 3
                             w_s = False
-                        if time_w <= now_time:
-                            xbee.atcmd('d3', OFF)
-                            w_s = True
-                            k_time = s_time + 5
-                elif wall == '22' and (d or everyday == '11'):  # 時間
-                    if o_time <= s_time <= c_time:
+                            t_w = True
+                if wall == '22' and (d or everyday == '11'):  # 時間
+                    if o_time <= s_time < c_time and p:
                         xbee.atcmd('d3', OFF)
                         xbee.atcmd('d2', ON)
-                        if p:
-                            print('t_open')
-                            p = False
-                    elif s_time >= c_time:
+                        # if p:
+                        off_time = time.ticks_ms() + 28000
+                        print('time_open')
+                        p = False
+                        o = True
+                    if s_time >= c_time and not p:
                         xbee.atcmd('d2', OFF)
                         xbee.atcmd('d3', ON)
-                        print('t_close')
+                        off_time = time.ticks_ms() + 28000
+                        print('time_close')
                         p = True
+                        o = True
+                    print('off', off_time, 'tic', time.ticks_ms())
+                    if off_time <= time.ticks_ms() and o:
+                        xbee.atcmd('d2', OFF)
+                        xbee.atcmd('d3', OFF)
                         if everyday == '11':
                             d = True
-
-                        else:
+                        if everyday == '10' and p:
                             d = False
                             mes_c = "C0100001リモート ON"
+                        o = False
             mes_a = "{0:.1f}".format(temp_a) + '℃' + "\x00"
             mes_a = 'A01' + "{:05.1f}".format(temp_a) + '温度:' + mes_a
             print(s_time)
             print(mes_c)
             print(mes_a)
-            print('o:', o_time, 'c:', c_time)  #
+            print('o:', o_time, 'c:', c_time)
             try:
                 xbee.transmit(addr_coordinator, mes_c)
-                time.sleep(5)
                 xbee.transmit(addr_coordinator, mes_a)
-            except OSError as e5:
-                print('メッセージ送信失敗', e5)
-            if (s_time % 10) == 0:
-                time.sleep(5)
-                try:
-                    xbee.transmit(addr_coordinator, 'S')
-                    print('time calibration')
-                except OSError as e2:
-                    print('時刻取得失敗', e2)
-        """       
-            if now_time == 35340 and s_w == 0:
-                s_w = 1
-                try:
-                    xbee.transmit(addr_coordinator, 'S')
-                    print('time calibration')
-                except OSError as e6:
-                    print('時刻取得失敗', e6)
-        """
+            except:
+                xb_join()
+        if s_time == 1380:
+            try:
+                xbee.transmit(addr_coordinator, 'S')
+                print('time calibration')
+            except:
+                xb_join()
+                xbee.transmit(addr_coordinator, 'S')
+                print('time calibration')
 
 
-try:
-    main()
-except Exception as e:
-    print('other', e)
-    time.sleep(10)
-    machine.reset()
+# try:
+main()
+# except:
+#    time.sleep(3)
+#    machine.reset()
